6f1eec0334fe4a43cbbd4c997c6298f76b83fb73
jemhik <96153452+jemhik@users.noreply.github.com>
2022-05-26 15:31:45 +0300

    Add files via upload

---
+++ training_center.sql
@@ -0,0 +1,149 @@
+CREATE DATABASE  IF NOT EXISTS `training_center` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `training_center`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: training_center
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `courses` (
+  `teacher_id` int DEFAULT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `materials` varchar(1500) DEFAULT NULL,
+  `task` varchar(200) DEFAULT NULL,
+  `duration_en` varchar(20) DEFAULT NULL,
+  `duration_ua` varchar(20) DEFAULT NULL,
+  `number_of_students` int DEFAULT NULL,
+  PRIMARY KEY (`name`),
+  KEY `teacher_id` (`teacher_id`),
+  CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses`
+--
+
+LOCK TABLES `courses` WRITE;
+/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
+INSERT INTO `courses` VALUES (5,'c','c  for Begginers','What is .Net Framework?\r .Net Framework is a software development platform developed by Microsoft for building and running Windows applications. The .Net framework consists of developer tools, programming languages, and libraries to build desktop and web applications. It is also used to build websites, web services, and games.\r \r The .Net framework was meant to create applications, which would run on the Windows Platform. The first version of the .Net framework was released in the year 2002. The version was called .Net framework 1.0. The Microsoft .Net framework has come a long way since then, and the current version is .Net Framework 4.7.2.\r The Microsoft .Net framework can be used to create both â Form-based and Web-based applications. Web services can also be developed using the .Net framework.\r The framework also supports various programming languages such as Visual Basic and C#. So developers can choose and select the language to develop the required application. In this chapter, you will learn some basics of the .Net framework.\r In this .Net Architecture tutorial, y','Crwating what type of applications .Net framework can be used?','8 hrs','8 год',3),(2,'Java','Java course for beginners','First of all you must create java class\nThen declare the main method\nPrint your text into console','Write your program which print \"I love java\"','4 hrs','4 год',5),(2,'mysql','MySQL for beginners','MySQL is the most popular Open Source Relational SQL database management system.\n\r\nMySQL is one of the best RDBMS being used for developing web-based software applications.\n\r\nThis tutorial will give you quick start with MySQL and make you comfortable with MySQL programming.','Write a simple MySql query','6 hrs','6 год',3);
+/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student_courses`
+--
+
+DROP TABLE IF EXISTS `student_courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student_courses` (
+  `student_id` int DEFAULT NULL,
+  `teacher_id` int DEFAULT NULL,
+  `course_name` varchar(30) DEFAULT NULL,
+  `progress_en` varchar(20) DEFAULT NULL,
+  `progress_ua` varchar(20) DEFAULT NULL,
+  `student_answer` varchar(500) DEFAULT NULL,
+  `assessment` int DEFAULT NULL,
+  KEY `student_id` (`student_id`),
+  KEY `teacher_id` (`teacher_id`),
+  KEY `course_name` (`course_name`),
+  CONSTRAINT `student_courses_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `student_courses_ibfk_2` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `student_courses_ibfk_3` FOREIGN KEY (`course_name`) REFERENCES `courses` (`name`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student_courses`
+--
+
+LOCK TABLES `student_courses` WRITE;
+/*!40000 ALTER TABLE `student_courses` DISABLE KEYS */;
+INSERT INTO `student_courses` VALUES (3,2,'Java','ended','Закінчено','safvasbfba',10),(3,2,'mysql','Answer submitted','В прогресі','12313',0),(6,5,'c','Not started','Не розпочато','',0),(4,5,'c','Not started','Не розпочато','',0),(4,2,'Java','Not started','Не розпочато','',0);
+/*!40000 ALTER TABLE `student_courses` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `students_count` AFTER INSERT ON `student_courses` FOR EACH ROW BEGIN
+    UPDATE courses SET number_of_students = number_of_students + 1 WHERE name = NEW.course_name;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `role` varchar(15) DEFAULT NULL,
+  `isBlocked` tinyint(1) DEFAULT (false),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'first1','last1','user1@gmail.com','123','Admin',0),(2,'first2','last2','user2@gmail.com','123','Teacher',0),(3,'first3','last3','user3@gmail.com','123','Student',0),(4,'first4','last4','user4@gmail.com','123','Student',0),(5,'first5','last5','user5@gmail.com','123','Teacher',0),(6,'Andrii','Andruhiv','user6@gmail.com','123','Student',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'training_center'
+--
+
+--
+-- Dumping routines for database 'training_center'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-26 15:31:09
